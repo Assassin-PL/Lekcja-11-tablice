4befba1535e8b7d601a434d686fff74f9dd4daba
Assassin-PL <michael.za1995@gmail.com>
2024-06-29 12:55:28 +0200

    do zadania 4

--- Lekcja 11 tablice/Program.cs
+++ Lekcja 11 tablice/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Xml.Linq;
 using System;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 internal class Program
 {
@@ -22,10 +24,46 @@ internal class Program
         Random wylosowanaL = new Random();
         int wyborLiczby =wylosowanaL.Next(0, lista_naszych_gier.Length);
         Console.WriteLine($"nasza wylosowana gra to : {lista_naszych_gier[wyborLiczby]}");
-        Console.ReadKey();
 
         //Ćwiczenie 2
         //Napisać program w konsoli, który znajdzie największą wartość w tablicy i wypisze ją
         //w konsoli.
+        // Program daniela: 
+        int[] numery = { 5, 3, 9, 2, 8, 1, 7, 6 };
+
+        int MaksymalnaWartosc = numery[0];
+
+        for (int i = 0; i < numery.Length; i++)
+        {
+            if (numery[i] > MaksymalnaWartosc)
+            {
+                MaksymalnaWartosc = numery[i];
+            }
+        }
+        Console.WriteLine($"Największa wartość w tablicy to: {MaksymalnaWartosc}");
+
+        string[] lodowka = { "mleko", "ser", "szynka", "pomidor", "jajko", "salata" };
+        // petla for each:
+        foreach (string produkt in lodowka)
+        {
+            Console.WriteLine($"jednen z naszych produktow w lodowce to: {produkt}");            
+        }
+        //Ćwiczenie 3
+        //Zmodyfikować kod ćwiczenia nr 2, tak by wykorzystywał pętlę foreach.
+        Console.WriteLine("znajdz najwieksza wartosc z     tablicy przy pomocy petli foreach");
+        foreach (int liczba in numery)
+        {
+            if (liczba > MaksymalnaWartosc)
+            {
+                MaksymalnaWartosc = liczba;
+            }
+        }
+        Console.WriteLine($"Najsza najwieksz liczba to : {MaksymalnaWartosc}");
+        //Ćwiczenie 4
+        //Napisać program konsolowy, który symuluje system rezerwacji biletów do kina.
+        //Należy stworzyć repertuar, pobrać od klienta dane osobowe(wybrany film, godzinę seansu, imię, nazwisko, telefon, mail oraz liczbę biletów) a następnie wyświetlić
+        //podsumowanie rezerwacji.
+        string[] filmy = { };
+        Console.ReadKey();
     }
 }
