acc4a5637a873ed231738ba719659d017ca87116
Assassin-PL <michael.za1995@gmail.com>
2024-06-29 12:25:06 +0200

    do cwiczenia nr 2

--- Lekcja 11 tablice/Program.cs
+++ Lekcja 11 tablice/Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿using System.Xml.Linq;
+using System;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] ucz ={ 6, 5, 4, 3, 2 };
+        string[] lista_naszych_gier = { "minecraft", "fifa", "my summer car", "long drive" };
+        //zad 1
+        //Stworzyć program konsolowy, w którym należy dodać tablicę 7 elementową,
+        //jako elementy należy dodać tytuły ulubionych gier i wyświetlić w konsoli każdy
+        //element.
+        //Console.WriteLine(lista_naszych_gier[0]);
+        //Console.WriteLine(lista_naszych_gier[1]);
+        //Console.WriteLine(lista_naszych_gier[2]);
+        //Console.WriteLine(lista_naszych_gier[3]);
+        for ( int i = 2; i < lista_naszych_gier.Length; i++)
+        {
+            Console.WriteLine($"nasza gra to: {lista_naszych_gier[i]}");
+        }
+        Random wylosowanaL = new Random();
+        int wyborLiczby =wylosowanaL.Next(0, lista_naszych_gier.Length);
+        Console.WriteLine($"nasza wylosowana gra to : {lista_naszych_gier[wyborLiczby]}");
+        Console.ReadKey();
+
+        //Ćwiczenie 2
+        //Napisać program w konsoli, który znajdzie największą wartość w tablicy i wypisze ją
+        //w konsoli.
     }
 }
